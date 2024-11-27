2b11dc4d7fd61b252d1d479d30de50273de39c47
JihoonRyu00 <harryryujh@naver.com>
2024-11-27 10:27:46 +0000

    fix-32: delete 'generate_recommendations' func
    #32

--- src/Light GCN/recommendation.py
+++ src/Light GCN/recommendation.py
@@ -1,22 +1,6 @@
 import torch
 import pandas as pd
 import os
-from train import get_predictions
-
-def generate_recommendations(model, adj_matrix, num_users, num_items, user_interactions, device, k=10):
-    model.eval()
-    recommendations = {}
-    
-    predictions = get_predictions(model, adj_matrix, num_users, num_items, device, k)
-    
-    # 이미 본 영화 제외
-    for user in range(num_users):
-        watched_items = user_interactions.get(user, set())
-        user_preds = predictions[user]
-        filtered_preds = [item for item in user_preds if item not in watched_items]
-        recommendations[user] = filtered_preds[:k]
-    
-    return recommendations
 
 def create_submission(recommendations, user_id_map, item_id_map, output_path):
     users = []
