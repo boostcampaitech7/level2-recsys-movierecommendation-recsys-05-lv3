d4558c0f28f84079dbcdcecb5879f5f9faa876fe
JihoonRyuu00 <harryryujh@naver.com>
2024-12-17 10:15:21 +0900

    mod-64: Dostring utils.py

--- src/LightGCN/utils.py
+++ src/LightGCN/utils.py
@@ -5,7 +5,22 @@ import scipy.sparse as sp
 from torch.utils.data import Dataset, DataLoader
 
 class TrainDataset(Dataset):
+    """
+    학습용 데이터셋 클래스
+
+    이 클래스는 PyTorch의 Dataset을 상속받아 LightGCN 모델 학습을 위한 데이터셋을 생성합니다.
+
+    매개변수:
+        df (pandas.DataFrame): 사용자-아이템 상호작용 데이터프레임
+        num_items (int): 전체 아이템 수
+
+    속성:
+        users (numpy.array): 사용자 ID 배열
+        pos_items (numpy.array): 긍정적 아이템 ID 배열
+        num_items (int): 전체 아이템 수
+    """
     def __init__(self, df, num_items):
+
         self.users = df['user'].values
         self.pos_items = df['item'].values
         self.num_items = num_items
@@ -22,6 +37,19 @@ class TrainDataset(Dataset):
         return user, pos_item, neg_item
 
 def create_adj_matrix(df, num_users, num_items):
+    """
+    정규화된 인접 행렬 생성 함수
+
+    사용자-아이템 상호작용 데이터를 기반으로 정규화된 인접 행렬을 생성합니다.
+
+    매개변수:
+        df (pandas.DataFrame): 사용자-아이템 상호작용 데이터프레임
+        num_users (int): 전체 사용자 수
+        num_items (int): 전체 아이템 수
+
+    반환값:
+        torch.sparse.FloatTensor: 정규화된 인접 행렬의 희소 텐서 표현
+    """
     user_nodes = df['user'].values
     item_nodes = df['item'].values + num_users
     edge_index = np.array([np.concatenate([user_nodes, item_nodes]),
@@ -42,6 +70,20 @@ def create_adj_matrix(df, num_users, num_items):
     )
 
 def create_submission(recommendations, user_id_map, item_id_map, output_path):
+    """
+    추천 결과를 제출 파일로 생성하는 함수
+
+    모델의 추천 결과를 CSV 형식의 제출 파일로 변환합니다.
+
+    매개변수:
+        recommendations (dict): 사용자별 추천 아이템 딕셔너리
+        user_id_map (pandas.DataFrame): 사용자 ID 매핑 정보
+        item_id_map (pandas.DataFrame): 아이템 ID 매핑 정보
+        output_path (str): 제출 파일 저장 경로
+
+    반환값:
+        None
+    """
     users = []
     items = []
     
